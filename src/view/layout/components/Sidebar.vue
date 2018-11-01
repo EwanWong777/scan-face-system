e69fdbbb54ef2192e131b618f9ce9e01cc060b2f
王少秋 <richard@wangshaoqiudeiMac.local>
2018-11-01 14:10:30 +0800

    201811011410

--- src/view/layout/components/Sidebar.vue
+++ src/view/layout/components/Sidebar.vue
@@ -10,11 +10,94 @@
                 </div>
             </div>
         </div>
+        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="sidebarCollapse">
+            <el-submenu index="1">
+                <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>导航一</span>
+                </template>
+                <el-menu-item-group>
+                    <template slot="title">分组一</template>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+            </el-menu-item>
+        </el-menu>
+        <ul class="m-menu">
+            <li v-for="(item) in menu" :key="item.id">
+                <div class="m-menu-first">
+                    <router-link :to="item.link">
+                        <div class="m-menu-link-icon" v-html="item.icon"></div>
+                        <div class="m-menu-link-text">
+                            {{item.title}}
+                        </div>
+                    </router-link>
+                </div>
+                <div class="m-menu-second">
+                    <router-link :to="item.children.link">
+                        <div class="m-menu-link-text">
+                            {{item.children.title}}
+                        </div>
+                    </router-link>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 <script>
 import { mapGetters } from "vuex";
 export default {
+    data() {
+        return {
+            menu: [
+                {
+                    id: 1,
+                    link: "/home",
+                    title: "主页",
+                    icon: '<i class="fas fa-home"></i>',
+                    children: [
+                        {
+                            id: 1 - 1,
+                            title: "菜单一一",
+                            link: "/home"
+                        }
+                    ]
+                },
+                {
+                    id: 2,
+                    link: "",
+                    title: "菜单一",
+                    icon: '<i class="fas fa-lock"></i>',
+                    children: [
+                        {
+                            id: 1 - 1,
+                            title: "菜单一一",
+                            link: "/home"
+                        }
+                    ]
+                }
+            ]
+        };
+    },
     computed: {
         ...mapGetters(["sidebarCollapse"])
     }
@@ -82,5 +165,18 @@ export default {
         margin-top 0
         height 0
         transition all 0.5s
+// 菜单
+.m-menu-first>a
+    display block
+    color $white0
+    line-height 60px
+    display flex
+.m-menu-link-icon
+    margin-left 20px
+.m-menu-link-text
+    margin-left 10px
+    white-space nowrap
+    overflow hidden
+    flex 1
 </style>
 
