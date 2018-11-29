329cb8fa235fbd57972d76e0248c4c848d735777
王少秋 <richard@wangshaoqiudeMacBook-Pro.local>
2018-11-29 22:01:53 +0800

    201811292001

---
+++ src/view/home/home.vue
@@ -0,0 +1,241 @@
+<template>
+    <div class="m-home">
+        <div class="m-card">
+            <el-row :gutter="20">
+                <el-col :span="6">
+                    <div class="m-card-panel">
+                        <div class="m-card-panel-icon">
+                            <i class="fas fa-user"></i>
+                        </div>
+                        <div class="m-card-panel-text">
+                            <h4>{{$t('home.user')}}</h4>
+                            <h1>10,000</h1>
+                        </div>
+                    </div>
+                </el-col>
+                <el-col :span="6">
+                    <div class="m-card-panel">
+                        <div class="m-card-panel-icon">
+                            <i class="fas fa-comment-dots"></i>
+                        </div>
+                        <div class="m-card-panel-text">
+                            <h4>{{$t('home.message')}}</h4>
+                            <h1>20</h1>
+                        </div>
+                    </div>
+                </el-col>
+                <el-col :span="6">
+                    <div class="m-card-panel">
+                        <div class="m-card-panel-icon">
+                            <i class="fas fa-yen-sign"></i>
+                        </div>
+                        <div class="m-card-panel-text">
+                            <h4>{{$t('home.earnings')}}</h4>
+                            <h1>88,888</h1>
+                        </div>
+                    </div>
+                </el-col>
+                <el-col :span="6">
+                    <div class="m-card-panel">
+                        <div class="m-card-panel-icon">
+                            <i class="fas fa-calendar-alt"></i>
+                        </div>
+                        <div class="m-card-panel-text">
+                            <h4>{{$t('home.date')}}</h4>
+                            <h1>{{date}}</h1>
+                        </div>
+                    </div>
+                </el-col>
+            </el-row>
+        </div>
+        <div class="m-chart">
+            <div class="m-chart-header">
+                <div class="m-chart-header-left">
+                    <div class="m-chart-title">
+                        {{$t('home.lineChart')}}
+                    </div>
+                </div>
+                <div class="m-chart-header-right">
+                    <div class="m-chart-form">
+                        <el-date-picker v-model="value1" type="date" placeholder="选择日期"></el-date-picker>
+                    </div>
+                </div>
+            </div>
+            <div class="m-chart-body">
+                <div ref="mLineChart" class="m-line-chart"></div>
+            </div>
+        </div>
+        <div class="m-chart">
+            <div class="m-chart-header">
+                <div class="m-chart-header-left">
+                    <div class="m-chart-title">
+                        {{$t('home.pieChart')}}
+                    </div>
+                </div>
+                <div class="m-chart-header-right">
+                    <div class="m-chart-form">
+                        <el-date-picker v-model="value2" type="date" placeholder="选择日期"></el-date-picker>
+                    </div>
+                </div>
+            </div>
+            <div class="m-chart-body">
+                <div ref="mPieChart" class="m-pie-chart"></div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            date: "",
+            value1: "",
+            value2: ""
+        };
+    },
+    mounted() {
+        this.date = this.initDate(new Date());
+        this.initLineChart();
+        this.initPieChart();
+        this.value1 = new Date();
+        this.value2 = new Date();
+    },
+    methods: {
+        // 格式化时间
+        initDate(date) {
+            var y = date.getFullYear();
+            var m = date.getMonth() + 1;
+            m = m < 10 ? "0" + m : m;
+            var d = date.getDate();
+            d = d < 10 ? "0" + d : d;
+            return y + "-" + m + "-" + d;
+        },
+        // 线图
+        initLineChart() {
+            let mLineChart = this.$echarts.init(this.$refs.mLineChart);
+            let option = {
+                legend: {
+                    data: ["销量"]
+                },
+                xAxis: {
+                    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                },
+                yAxis: {},
+                series: [
+                    {
+                        name: "销量",
+                        type: "line",
+                        data: [5, 20, 36, 10, 10, 20],
+                        smooth: true
+                    }
+                ],
+                color: [
+                    "#F44336",
+                    "#FF9800",
+                    "#FFEB3B",
+                    "#4CAF50",
+                    "#00BCD4",
+                    "#2196F3",
+                    "#9C27B0"
+                ]
+            };
+            mLineChart.setOption(option);
+        },
+        // 饼图
+        initPieChart() {
+            let mPieChart = this.$echarts.init(this.$refs.mPieChart);
+            let option = {
+                tooltip: {
+                    trigger: "item",
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                legend: {
+                    orient: "vertical",
+                    left: "left",
+                    data: [
+                        "直接访问",
+                        "邮件营销",
+                        "联盟广告",
+                        "视频广告",
+                        "搜索引擎"
+                    ]
+                },
+                series: [
+                    {
+                        name: "访问来源",
+                        type: "pie",
+                        radius: "55%",
+                        center: ["50%", "60%"],
+                        data: [
+                            { value: 335, name: "直接访问" },
+                            { value: 310, name: "邮件营销" },
+                            { value: 234, name: "联盟广告" },
+                            { value: 135, name: "视频广告" },
+                            { value: 1548, name: "搜索引擎" }
+                        ],
+                        itemStyle: {
+                            emphasis: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: "rgba(0, 0, 0, 0.5)"
+                            }
+                        }
+                    }
+                ],
+                color: [
+                    "#F44336",
+                    "#FF9800",
+                    "#FFEB3B",
+                    "#4CAF50",
+                    "#00BCD4",
+                    "#2196F3",
+                    "#9C27B0"
+                ]
+            };
+            mPieChart.setOption(option);
+        }
+    }
+};
+</script>
+<style lang="stylus" scoped>
+@import '../../style/variables.styl'
+.m-home
+    padding 20px
+.m-card
+    margin-bottom 20px
+.m-card-panel
+    background-color $white0
+    padding 20px
+    display flex
+.m-card-panel-icon
+    color $base5
+    font-size 60px
+    width 60px
+    text-align center
+    margin-right 20px
+.m-card-panel-text
+    flex 1
+    text-align right
+    h4
+        margin-bottom 10px
+        color $font2
+.m-chart
+    background-color $white0
+    padding 20px
+    margin-bottom 20px
+.m-chart-header
+    display flex
+    margin-bottom 20px
+.m-chart-title
+    font-size 18px
+    font-weight bold
+    line-height 40px
+.m-chart-header-right
+    flex 1
+    display flex
+    justify-content flex-end
+.m-line-chart
+    height 400px
+.m-pie-chart
+    height 400px
+</style>
---
+++ src/view/login/login.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="m-login">
+        <div class="m-login-bg"></div>
+        <div class="m-login-container">
+            <div class="m-login-panel">
+                <div class="m-login-logo">
+                    <img src="@/assets/logo-orange.svg" alt="">
+                </div>
+                <div class="m-login-title">
+                    {{$t('login.title')}}
+                </div>
+                <el-form>
+                    <el-form-item :class="errorBags.has('username')?'is-error':''">
+                        <el-input v-validate="{ required: true, regex: /^[a-zA-Z0-9_-]{4,16}$/ }" :data-vv-as="$t('login.username')" v-model="loginForm.username" name="username" :placeholder="$t('login.usernamePlaceholder')" auto-complete="on" class="m-login-input">
+                        </el-input>
+                        <div class="m-form-error" v-show="errorBags.has('username')">{{ errorBags.first('username') }}</div>
+                    </el-form-item>
+                    <el-form-item :class="errorBags.has('password')?'is-error':''">
+                        <el-input type="password" v-validate="{ required: true, regex: /^[a-zA-Z0-9_.@~!?]{8,17}$/ }" :data-vv-as="$t('login.password')" v-model="loginForm.password" name="password" :placeholder="$t('login.passwordPlaceholder')" auto-complete="on" class="m-login-input" @keyup.enter.native="handleLoginByUserName">
+                        </el-input>
+                        <div class="m-form-error" v-show="errorBags.has('password')">{{ errorBags.first('password') }}</div>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" class="m-login-btn" @click="handleLoginByUserName">{{$t('login.login')}}</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { mapActions } from "vuex";
+export default {
+    data() {
+        return {
+            loginForm: {
+                username: "",
+                password: ""
+            }
+        };
+    },
+    methods: {
+        ...mapActions(["loginByUserName"]),
+        handleLoginByUserName: function() {
+            this.$validator.validateAll().then(result => {
+                if (result) {
+                    this.loginByUserName(this.loginForm)
+                        .then(res => {
+                            this.$router.push({ path: this.redirect || "/" });
+                            if (res) {
+                                this.$message.error(res);
+                            }
+                        })
+                        .catch(() => {
+                            this.$message.error("网络错误");
+                        });
+                } else {
+                    this.$message.error("输入内容格式错误");
+                }
+            });
+        }
+    }
+};
+</script>
+<style lang="stylus" scoped>
+@import '../../style/variables.styl'
+.m-login
+    height 100%
+    position relative
+.m-login-bg
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    background #212121
+.m-login-container
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    display flex
+    justify-content center
+    align-items center
+.m-login-panel
+    width 500px
+    background-color $white0
+    padding 60px
+    border-radius 10px
+.m-login-logo
+    text-align center
+    margin-bottom 20px
+    img
+        width 120px
+.m-login-title
+    text-align center
+    font-size 28px
+    font-weight bold
+    margin-bottom 40px
+.m-form-error
+    color $danger5
+.m-login-btn
+    width 100%
+</style>
